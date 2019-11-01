0ae13e38e20855ea4f8bfdde679a3e8d01e1bcc0
lemered <lemered@hotmail.com>
2019-11-01 14:43:14 -0400

    Added method transferToSalesVan to SalesVan.java

--- src/SalesVan.java
+++ src/SalesVan.java
@@ -1,11 +1,19 @@
 import java.util.ArrayList;
 
-public class SalesVan {
+public class SalesVan extends ArrayList{
     public ArrayList<BikePart> vanList;
 
     public SalesVan(ArrayList<BikePart> vanList){
         this.vanList=vanList;
     }
 
+    public void deliverToWarehouse(String warehouseInventoryName){
 
+    }
+
+    public void transferToSalesVan(SalesVan altSalesVan){
+        for (int i = 0; i < vanList.size(); i++) {
+            altSalesVan.add(vanList.get(i));
+        }
+    }
 }
