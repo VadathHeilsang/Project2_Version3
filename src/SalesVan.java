0ffb74de7ead980839f60d8bcac5cf04dea4323b
lemered <lemered@hotmail.com>
2019-11-01 15:22:11 -0400

    Finished methods for SalesVanClass

--- src/SalesVan.java
+++ src/SalesVan.java
@@ -7,13 +7,17 @@ public class SalesVan extends ArrayList{
         this.vanList=vanList;
     }
 
-    public void deliverToWarehouse(String warehouseInventoryName){
-
+    public void getFromWarehouse(ArrayList<BikePart> warehouseInventoryName){
+        for (int i = 0; i < warehouseInventoryName.size(); i++) {
+            vanList.add(warehouseInventoryName.get(i));
+            warehouseInventoryName.remove(i);
+        }
     }
 
-    public void transferToSalesVan(SalesVan altSalesVan){
+    public void transferTo(ArrayList<BikePart> altBikePartArrayList){
         for (int i = 0; i < vanList.size(); i++) {
-            altSalesVan.add(vanList.get(i));
+            altBikePartArrayList.add(vanList.get(i));
+            vanList.remove(i);
         }
     }
 }
